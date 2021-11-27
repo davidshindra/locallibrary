848d2b180c3313cda44d4c6c15d65170e8241dc2
Shindra David <davidshindra.dev@gmail.com>
2021-11-27 22:02:55 +0300

    created catalog home view

--- apps/catalog/admin.py
+++ apps/catalog/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
+from .models import Author, Category, Book, BookCopy, Language
+
 # Register your models here.
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(BookCopy)
+class BookCopyAdmin(admin.ModelAdmin):
+    pass
--- apps/catalog/views.py
+++ apps/catalog/views.py
@@ -1,3 +1,22 @@
+from django.db.models.query import QuerySet
 from django.shortcuts import render
+from django.views import View
+from .models import Author, Book, BookCopy, Category
 
 # Create your views here.
+
+class HomeView(View):
+    def get(self, request):
+        num_authors = Author.objects.all().count()
+        num_books = Book.objects.all().count()
+        num_copies = BookCopy.objects.all().count()
+        num_copies_available = BookCopy.objects.filter(status__iexact='Available')
+        num_categories = Category.objects.all().count()
+        context = {
+            'num_authors': num_authors,
+            'num_books': num_books,
+            'num_copies': num_copies,
+            'num_copies_available': num_copies_available,
+            'num_categories': num_categories
+        }
+        return render(request, 'catalog/home.html', context)
